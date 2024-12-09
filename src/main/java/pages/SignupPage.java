0d511d4a31c272366e2d2525694703816151b4c0
QualityAssuranceEmad <emad.maher.tester@gmail.com>
2024-12-09 10:46:13 +0200

    creating Sign up page elment locators

--- src/main/java/pages/SignupPage.java
+++ src/main/java/pages/SignupPage.java
@@ -1,4 +1,20 @@
 package pages;
 
+import com.shaft.driver.SHAFT;
+import org.openqa.selenium.By;
+
 public class SignupPage {
+    private SHAFT.GUI.WebDriver driver;
+    public SignupPage(SHAFT.GUI.WebDriver driver) {
+        this.driver = driver;
+    }
+    By First_Name = By.id("firstName");
+    By Last_Name = By.id("lastName");
+    By Email = By.id("email");
+    By Phone = By.id("phone");
+    By Password = By.id("password1");
+    By Confirm_Password = By.id("password2");
+    By CheckBox=By.xpath("(//div[@class='p-checkbox-box'])[2]");
+    By Sin_Up=By
+
 }
